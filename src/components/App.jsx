546c610cfe0e2147dc044ffaf1e4f44c5fde4128
AsyaDev <142207257+AsyaDev14@users.noreply.github.com>
2023-11-30 22:40:43 +0200

    add all components && css modules

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 import user from './Data/user.json';
 import { Profile } from './Profile';
+import { Statistics } from './Statistics';
+import data from './Data/data.json';
+import { FriendList } from './FriendList';
+import friends from './Data/friends.json'
+import { TransactionHistory } from './TransactionHistory';
+import transactions from './Data/transactions.json'
 
 const { username, tag, location, avatar, stats } = user;
 export const App = () => {
@@ -13,6 +19,10 @@ export const App = () => {
         avatar={avatar}
         stats={stats}
       />
+
+      <Statistics title="Upload stats" stats={data} />
+       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </>
   );
 };
