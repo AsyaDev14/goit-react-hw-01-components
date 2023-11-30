46e6c974c2d9ccf98df0469f348b89e58dc827bb
AsyaDev <142207257+AsyaDev14@users.noreply.github.com>
2023-11-30 22:09:44 +0200

    add profile component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import React from 'react';
+import user from './Data/user.json';
+import { Profile } from './Profile';
+
+const { username, tag, location, avatar, stats } = user;
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+         <Profile
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
+    </>
   );
 };
